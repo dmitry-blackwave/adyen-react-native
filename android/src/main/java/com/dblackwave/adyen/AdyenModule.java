5f03d02d80022134e46d973267f8e7a46613def7
Dmitry Belov <dmitriy.belov@sportpursuit.co.uk>
2019-02-14 17:00:11 +0700

    init

---
+++ android/src/main/java/com/dblackwave/adyen/AdyenModule.java
@@ -0,0 +1,55 @@
+package com.dblackwave.adyen;
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.support.annotation.Nullable;
+
+import com.facebook.common.executors.UiThreadImmediateExecutorService;
+import com.facebook.common.internal.Files;
+import com.facebook.common.references.CloseableReference;
+import com.facebook.common.util.UriUtil;
+import com.facebook.datasource.DataSource;
+import com.facebook.drawee.backends.pipeline.Fresco;
+import com.facebook.imagepipeline.common.ResizeOptions;
+import com.facebook.imagepipeline.core.ImagePipeline;
+import com.facebook.imagepipeline.datasource.BaseBitmapDataSubscriber;
+import com.facebook.imagepipeline.image.CloseableImage;
+import com.facebook.imagepipeline.request.ImageRequest;
+import com.facebook.imagepipeline.request.ImageRequestBuilder;
+import com.facebook.react.bridge.Arguments;
+import com.facebook.react.bridge.Callback;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.ReadableMap;
+import com.facebook.react.bridge.WritableMap;
+import com.facebook.react.modules.core.DeviceEventManagerModule;
+
+import com.adyen.core.PaymentRequest;
+import com.adyen.core.interfaces.HttpResponseCallback;
+import com.adyen.core.interfaces.PaymentDataCallback;
+import com.adyen.core.interfaces.PaymentRequestListener;
+import com.adyen.core.models.Payment;
+import com.adyen.core.models.PaymentRequestResult;
+import com.adyen.core.utils.AsyncHttpClient;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.nio.charset.Charset;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+
+import java.io.File;
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.UUID;
+import java.io.StringWriter;
+import java.io.PrintWriter;
+
+public class AdyenModule extends ReactContextBaseJavaModule {
+    private PaymentRequest paymentRequest;
+}
