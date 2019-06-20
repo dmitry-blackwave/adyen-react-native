7639db1e3ad1378eda049c1120db22a39b10b3ea
Joe Patterson <joe.patterson@dominos.com.au>
2019-06-20 16:13:17 +1000

    add types

--- index.d.ts
+++ index.d.ts
@@ -4,3 +4,6 @@ export function onPaymentResult(
   callback: (code: any, error: any) => void
 ): void;
 export function confirmPayment(token: string): void;
+export function onRequestPaymentSession(
+  callback: (token: string, returnUrl: string) => void
+): void;
